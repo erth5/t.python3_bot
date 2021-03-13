05867e9e38331a27c69aed1f718978429983b239
Rthem@gmx.de <ma51adof>
2021-03-13 20:57:46 +0100

    change to better wrapper

--- main.py
+++ main.py
@@ -1,14 +1,52 @@
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
+# mastrobot_example.py
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 
-def hello(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(f'Hello {update.effective_user.first_name}')
+# function to handle the /start command
+def start(update, context):
+    update.message.reply_text('start command received')
 
 
-updater = Updater('YOUR TOKEN HERE')
+# function to handle the /help command
+def help(update, context):
+    update.message.reply_text('help command received')
 
-updater.dispatcher.add_handler(CommandHandler('hello', hello))
 
-updater.start_polling()
-updater.idle()
+# function to handle errors occured in the dispatcher
+def error(update, context):
+    update.message.reply_text('an error occured')
+
+
+# function to handle normal text
+def text(update, context):
+    text_received = update.message.text
+    update.message.reply_text(f'did you said "{text_received}" ?')
+
+
+def main():
+    TOKEN = ""
+
+    # create the updater, that will automatically create also a dispatcher and a queue to
+    # make them dialoge
+    updater = Updater(TOKEN, use_context=True)
+    dispatcher = updater.dispatcher
+
+    # add handlers for start and help commands
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("help", help))
+
+    # add an handler for normal text (not commands)
+    dispatcher.add_handler(MessageHandler(Filters.text, text))
+
+    # add an handler for errors
+    dispatcher.add_error_handler(error)
+
+    # start your shiny new bot
+    updater.start_polling()
+
+    # run the bot until Ctrl-C
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
